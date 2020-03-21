7b467f9e9a4da034b490a017945ba86aa023e164
Zhenzhen Wang <zwang211@dons.usfca.edu>
2020-03-20 19:24:24 -0700

    Add tooltips

--- javascript/prototype3.js
+++ javascript/prototype3.js
@@ -1,66 +1,66 @@
 
 // set the dimensions and margins of the graph
-var margin = {top: 30, right: 30, bottom: 30, left: 100},
-  width = 960 - margin.left - margin.right,
-  height = 300 - margin.top - margin.bottom;
+var margin = { top: 30, right: 30, bottom: 30, left: 100 },
+    width = 960 - margin.left - margin.right,
+    height = 300 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#prototype3")
-.append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-.append("g")
-  .attr("transform",
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
         "translate(" + margin.left + "," + margin.top + ")");
 
 // append the svg object to the body of the page
 var svg2 = d3.select("#prototype3")
-.append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-.append("g")
-  .attr("transform",
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
         "translate(" + margin.left + "," + margin.top + ")");
 
 // Labels of row and columns
-var calltypes = ['Alarm', 'Fire', 'Potentially Life-Threatening', 'Non Life-Threatening']
+var calltypes = ['Alarm', 'Fire', 'Potentially Life-Threatening', 'Non Life-threatening']
 var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 var hoods = ['Hayes Valley', 'Mission', 'South of Market', 'Tenderloin', 'Western Addition']
 
 // Build heatmap scales and axis:
 var x = d3.scaleBand()
-  .range([ 0, width ])
-  .domain(months)
-  .padding(0.01);
+    .range([0, width])
+    .domain(months)
+    .padding(0.01);
 svg.append("g")
-  .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x))
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
 
 // Build X scales and axis:
 var y = d3.scaleBand()
-  .range([ height, 0 ])
-  .domain(hoods)
-  .padding(0.01);
+    .range([height, 0])
+    .domain(hoods)
+    .padding(0.01);
 svg.append("g")
-  .call(d3.axisLeft(y));
+    .call(d3.axisLeft(y));
 
 // Build barchart scales and axis:
 var x2 = d3.scaleBand()
-  .range([ 0, width ])
-  .domain(calltypes)
-  .padding(0.01);
+    .range([0, width])
+    .domain(calltypes)
+    .padding(0.01);
 svg2.append("g")
-  .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x2))
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x2))
 
 var y2 = d3.scaleLinear()
-  .range([ height, 0 ])
-  .domain([0, 20])
+    .range([height, 0])
+    .domain([0, 20])
 svg2.append("g")
-  .call(d3.axisLeft(y2))
+    .call(d3.axisLeft(y2))
 
-// svg.append('g_heat');
-// svg.append("gbar");
+svg.append('g').attr('id', 'chart');
+svg2.append('g').attr('id', 'chart');
 
 var rawcsv;
 
@@ -73,10 +73,10 @@ function query(data, calltype, month, hood) {
             [row.month, ''].includes(month) &&
             [row.hood, ''].includes(hood)) {
             weightedsum += row.avgtime * row.count;
-            count += 1*row.count;
+            count += 1 * row.count;
         }
     }
-    avg = count ? weightedsum/count : 0;
+    avg = count ? weightedsum / count : 0;
     return [avg, count];
 }
 var selected_calltype = ''
@@ -99,7 +99,6 @@ function filter_heat() {
         }
     }
 }
-
 function filter_bar() {
     for (calltype of calltypes) {
         [avgtime, count] = query(rawcsv, calltype, selected_month, selected_hood)
@@ -113,39 +112,84 @@ function filter_bar() {
 
 // Build color scale
 var myColor = d3.scaleLinear()
-  .range(["white", "#69b3a2"])
-  .domain([5, 10])
+    .range(["white", "#69b3a2"])
+    .domain([3, 10])
 
-  function redraw() {
+function ttip(d) {
+    let div = d3.select("body").append("div")
+        .attr("id", "details")
+        .attr("class", "tooltip")
+        .style("left", d3.event.pageX + "px")
+        .style("top", d3.event.pageY + "px")
+
+    let rows = div.append("table")
+        .selectAll("tr")
+        .data(Object.keys(d))
+        .enter()
+        .append("tr");
+
+    rows.append("th").text(key => key);
+    rows.append("td").text(key => d[key]);
+}
+
+function redraw() {
+    d3.selectAll("div#details").remove();
     data = rawcsv;
 
     // Heatmap
-    svg.append('g').selectAll('rect')
-      .data(dataheat, function(d) {return d.group+':'+d.variable;})
-      .join("rect")
-      .attr("x", function(d) { return x(d.month) })
-      .attr("y", function(d) { return y(d.hood) })
-      .attr("width", x.bandwidth() )
-      .attr("height", y.bandwidth() )
-      .style("fill", function(d) { return myColor(d.avgtime)} );
+    svg.select("g#chart").selectAll('rect')
+        .data(dataheat, function (d) { return d.group + ':' + d.variable; })
+        .join("rect")
+        .attr("x", function (d) { return x(d.month) })
+        .attr("y", function (d) { return y(d.hood) })
+        .attr("width", x.bandwidth())
+        .attr("height", y.bandwidth())
+        .style("fill", function (d) { return myColor(d.avgtime) });
+
+    rects = svg.select("g#chart").selectAll("rect");
+    rects.on("mouseover", function (d) {
+        d3.select(this)
+            .raise() // bring to front
+            .style("stroke", "red")
+            .style("stroke-width", 2);
+        ttip(d);
+    });
+    rects.on("mouseout", function (d) {
+        d3.select(this).style("stroke", null);
+        d3.selectAll("div#details").remove();
+    });
 
     // Bar chart
-    svg2.append('g').selectAll('rect')
-      .data(databar, function(d) {return d.group+':'+d.variable;})
-      .join("rect")
-      .attr("x", function(d) { return x2(d.calltype) })
-      .attr("y", function(d) { return y2(d.avgtime) })
-      .attr("width", x2.bandwidth() )
-      .attr("height", function(d) {return height - y2(d.avgtime) } )
-      .style("fill", function(d) { return myColor(d.avgtime)} );
-  }
+    svg2.select("g#chart").selectAll('rect')
+        .data(databar, function (d) { return d.group + ':' + d.variable; })
+        .join("rect")
+        .attr("x", function (d) { return x2(d.calltype) })
+        .attr("y", function (d) { return y2(d.avgtime) })
+        .attr("width", x2.bandwidth())
+        .attr("height", function (d) { return height - y2(d.avgtime) })
+        .style("fill", function (d) { return myColor(d.avgtime) });
+
+    rects = svg2.select("g#chart").selectAll("rect");
+    rects.on("mouseover.highlight", function (d) {
+        d3.select(this)
+            .raise() // bring to front
+            .style("stroke", "red")
+            .style("stroke-width", 2);
+        ttip(d);
+        d3.select(status).text("highlight: " + d.letter);
+    });
+    rects.on("mouseout.highlight", function (d) {
+        d3.select(this).style("stroke", null);
+        d3.selectAll("div#details").remove();
+    });
+}
 
 //Read the data
 d3.tsv("station36.csv")
-  .then(function(data) {
-    rawcsv = data;
-    console.log(query(data, 'Fire', 'January', 'Tenderloin'))
-    filter_heat();
-    filter_bar();
-    redraw();
-})
+    .then(function (data) {
+        rawcsv = data;
+        console.log(query(data, 'Fire', 'January', 'Tenderloin'))
+        filter_heat();
+        filter_bar();
+        redraw();
+    })
